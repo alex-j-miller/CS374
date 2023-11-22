c5ba03f699c65fe2a0d81ef12f46e2a5f52ab338
alex-j-miller <alexiscool2003@sbcglobal.net>
2023-11-21 21:13:23 -0500

    hw04

--- Homework04/parallel binary/parSquareSum.cpp
+++ Homework04/parallel binary/parSquareSum.cpp
@@ -13,17 +13,17 @@
  * Homework 04
  */
 
-#include <stdio.h>      /* I/O */
-#include <stdlib.h>     /* calloc(), exit(), etc. */
+#include <stdio.h> /* I/O */
+#include <stdlib.h> /* calloc(), exit(), etc. */
 #include <mpi.h>
 #include <vector> 
-#include "OO_MPI_IO.h"          // ParallelReader
+#include "OO_MPI_IO.h" // ParallelReader
 
 
 typedef double Item;
 
 void readArray(char * fileName, Item ** a, int * n);
-double arraySquareAndSum(std::vector<double> val, int numValues);
+double vectorSquareAndSum(std::vector<double> val, int numValues, int id, int numProcs);
 char* processCmdLineArgs(int argc, char** argv);
 void check(FILE* fptr, char* fileName);
 long getFileSize(FILE* fPtr);
@@ -38,8 +38,7 @@ int main(int argc, char * argv[])
   double sumTime = 0.0;
   double sumStart = 0.0;
   const int MASTER = 0;
-  int id = -1;
-  int numProcs = -1;
+  int id = -1, numProcs = -1;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
@@ -54,10 +53,7 @@ int main(int argc, char * argv[])
   std::vector<double> vec;
 
   reader.readChunk(vec);
-
-  long numItems = reader.getNumItemsInFile();  // integer division
-  Item* arrayPtr = (Item*) malloc( numItems*sizeof(Item) );
-
+  long numItems = reader.getNumItemsInFile();
 
   if (argc != 2) {
     fprintf(stderr, "\n*** Usage: squareAndSum <inputFile>\n\n");
@@ -66,20 +62,20 @@ int main(int argc, char * argv[])
   readTime = MPI_Wtime() - startTime;
   sumStart = MPI_Wtime();
 
-  sum = arraySquareAndSum(vec, numItems);
+  sum = vectorSquareAndSum(vec, numItems, id, numProcs);
 
 
   sumTime = MPI_Wtime() - sumStart;
   totalTime = readTime + sumTime;
+
   MPI_Reduce(&sum, &totalSum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
   reader.close();
 
 if(id == MASTER) {
-  printf("The sum of the squares of the values in the file '%s' is %g\n", argv[1], totalSum);
+  printf("\nThe sum of the squares of the values in the file '%s' is %g\n", argv[1], totalSum);
   printf("Read Time:\tSum Time:\tTotal Time:\n%f \t%f  \t%f\n", readTime, sumTime, totalTime);
 }
 
-  free(arrayPtr);
   MPI_Finalize();
   return 0;
 }
@@ -101,17 +97,18 @@ char* processCmdLineArgs(int argc, char** argv) {
 }
 
 
-/* arraySquareAndSum sums the squares of the values
- *  in an array of numeric Items.
- * Receive: a, a pointer to the head of an array of Items;
- *          numValues, the number of values in the array.
- * Return: the sum of the values in the array.
+/* vectorSquareAndSum sums the squares of the values
+ *  in an vector of numeric Items.
+ * Receive: val, a pointer to the head of an vector of Items;
+ *          numValues, the number of values in the vector.
+ *          id, process's id.
+ *          numProcs, number of processes.
+ * Return: the sum of the values in the vector.
  */
-
-Item arraySquareAndSum(std::vector<double> val, int numValues) {
+Item vectorSquareAndSum(std::vector<double> val, int numValues, int id, int numProcs) {
   Item result = 0.0;
 
-  for (int i = 0; i < numValues; ++i) {
+  for (int i = 0; i < numValues/numProcs; i++) {
     result += (val[i] * val[i]);
   }
 
